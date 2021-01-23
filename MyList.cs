1779afd7d127111cba043a8fcafc231b31dd4ba2
UmutBatuhanALPAN <64593480+ubacodes@users.noreply.github.com>
2021-01-23 20:47:18 +0300

    Yazılım Geliştici Yetiştirme Kampı
    Generic yapısını anlamak için ödev.

---
+++ MyList.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics
+{
+    class MyList<T> // istediğimizi yazabiliriz ama T = Type'dan gelir.
+    {
+        T[] _array;
+        T[] _tempArray;
+        public MyList()
+        {
+            _array = new T[0];
+        }
+        public void Add(T item) // parametre olarak istediğimiz veri tipi ile çalışabilir
+        {
+            _tempArray = _array;
+            _array = new T[_array.Length + 1];
+
+            for (int i = 0; i < _tempArray.Length; i++)
+            {
+                _array[i] = _tempArray[i];
+            }
+            _array[_array.Length - 1] = item;
+        }
+
+        public void View()
+        {
+            foreach (T arrayItems in _array)
+            {
+                Console.WriteLine(arrayItems);
+            }
+        }
+        private int _count;
+
+        public int MyCount
+        {
+            get { return _array.Length; }  // dizinin uzunluğunu yolla sadece get edebil yani readonly
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Generic'ler hangi veri tipini girersen içerisindeki fonksiyonlar parametre olarak o veri tipini istiyorlar
+            // List<string> sehirler = new List<string>(); // Generic yapıda klasik bir List nesnesi
+            // metotlar kullanılırken () işareti olur eğer yoksa property dir
+            MyList<string> sehirler2 = new MyList<string>();
+            string eleman = "";
+            string deger = "";
+            while (true)
+            {
+                Console.WriteLine("Devam edilsin mi?(q = çıkış) ");
+                deger = Console.ReadLine();
+                if (deger == "q" || deger == "Q")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Eklenecek olan elemanı girin: ");
+                    eleman = Console.ReadLine();
+                    sehirler2.Add(eleman);
+                }
+            }
+
+            sehirler2.View();
+
+            Console.WriteLine("Liste içerisindeki eleman sayısı: "+sehirler2.MyCount);
+
+        }
+    }
+}
